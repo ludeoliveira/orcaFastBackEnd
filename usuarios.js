10c35e4dc66d99be15945ab5bc4023f8b3191bd6
Lu de Oliveira <luludeolive.dev@gmail.com>
2022-07-20 17:14:22 -0300

    instala pacotes

---
+++ usuarios.js
@@ -0,0 +1,23 @@
+const express  = require('express') 
+const app = express() 
+const port = process.env.PORT 
+
+const pg = require('pg')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+// para ver o que vem do formulario do postman
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+
+
+const consStr =  process.env.DATABASE_URL
+const pool = new pg.Pool({ connectionString: consStr })
+
+
+
+
+app.listen(port, () => {
+    console.log(`executando em http://localhost/${port}`)
+})
