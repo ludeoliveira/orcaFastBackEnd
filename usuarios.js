c574501f14e91e84768ce685cfb426876b8231c1
Katiane Griza <katygriza@hotmail.com>
2022-07-22 12:02:41 -0300

    inicio login

--- usuarios.js
+++ usuarios.js
@@ -65,6 +65,59 @@ app.post("/usuarios", (req, res) => {
   });
 });
 
+app.post("/usuarios/login", (req, res) => {
+  pool.connect((err, client) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Erro de conexão",
+      });
+    }
+    client.query('select * from usuario where cnpj = $1', [req.body.cnpj], (error, result) => {
+      if (error) {
+        return res.status(500).send({ message: 'Usuario não encontrado' })
+      }
+      if (result.rowCount) {
+        bcrypt.hash(req.body.senha, 10, (error, hash) => {
+          if (error) {
+            return res.status(500).send({ message: 'Falha na autenticação' })
+          }
+          var sql =
+            "insert into usuario (razaosocial, cnpj, email, senha, telefone, celular, cep, rua, numeroendereco, complementoendereco, bairro, cidade, estado, logo)values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)";
+          var dados = [
+            req.body.razaosocial,
+            req.body.cnpj,
+            req.body.email,
+            hash,
+            req.body.telefone,
+            req.body.celular,
+            req.body.cep,
+            req.body.rua,
+            req.body.numeroendereco,
+            req.body.complementoendereco,
+            req.body.bairro,
+            req.body.cidade,
+            req.body.estado,
+            req.body.logo,
+          ];
+          client.query(sql, dados, (error, result) => {
+            if (error) {
+              return res.status(500).send({
+                message: "Erro ao inserir o usuário",
+                error: error.message,
+              });
+            }
+            return res.status(201).send({
+              message: "Usuário cadastrado com sucesso",
+            });
+          });
+        });
+      }
+    })
+
+  });
+});
+
+
 app.put("/usuarios/:idusuario", (req, res) => {
   pool.connect((err, client) => {
     if (err) {
