8ad6739fe8d2eeb7c24ce08bd6793dc71ddb7d7b
Katiane Griza <katygriza@hotmail.com>
2022-07-22 17:28:57 -0300

    Criação do token

--- usuarios.js
+++ usuarios.js
@@ -72,51 +72,42 @@ app.post("/usuarios/login", (req, res) => {
         message: "Erro de conexão",
       });
     }
-    client.query('select * from usuario where cnpj = $1', [req.body.cnpj], (error, result) => {
+    client.query('select * from usuario where email = $1', [req.body.email], (error, result) => {
       if (error) {
         return res.status(500).send({ message: 'Usuario não encontrado' })
       }
-      if (result.rowCount) {
-        bcrypt.hash(req.body.senha, 10, (error, hash) => {
+      if (result.rowCount > 0) {
+        bcrypt.compare(req.body.senha, result.rows[0].senha, (error, results) => {
           if (error) {
             return res.status(500).send({ message: 'Falha na autenticação' })
           }
-          var sql =
-            "insert into usuario (razaosocial, cnpj, email, senha, telefone, celular, cep, rua, numeroendereco, complementoendereco, bairro, cidade, estado, logo)values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)";
-          var dados = [
-            req.body.razaosocial,
-            req.body.cnpj,
-            req.body.email,
-            hash,
-            req.body.telefone,
-            req.body.celular,
-            req.body.cep,
-            req.body.rua,
-            req.body.numeroendereco,
-            req.body.complementoendereco,
-            req.body.bairro,
-            req.body.cidade,
-            req.body.estado,
-            req.body.logo,
-          ];
-          client.query(sql, dados, (error, result) => {
-            if (error) {
-              return res.status(500).send({
-                message: "Erro ao inserir o usuário",
-                error: error.message,
-              });
-            }
-            return res.status(201).send({
-              message: "Usuário cadastrado com sucesso",
-            });
-          });
-        });
+          //Início do token
+          if (results) {
+            let token = jwt.sign({
+              razaosocial: result.rows[0].razaosocial,
+              cnpj: result.rows[0].cnpj,
+              email: result.rows[0].email
+            },
+              'segredo', { expiresIn: '1h' }
+            )
+            return res.status(200).send({
+              message: 'Conectado com sucesso',
+              token: token
+            })
+          }
+          return res.status(401).send({
+            message: 'Senha não confere'
+          })
+        })
+      }
+      else {
+        return res.status(401).send({
+          message: 'Usuário não encontrado'
+        })
       }
     })
-
-  });
-});
-
+  })
+})
 
 app.put("/usuarios/:idusuario", (req, res) => {
   pool.connect((err, client) => {
